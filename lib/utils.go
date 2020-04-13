dc3a3bfbed95f80008e46dbd8b9ffbdc23b404e0
Rodrigo Brito <rig.10@hotmail.com>
2020-04-13 20:15:44 -0300

    extract functions and interface

--- lib/utils.go
+++ lib/utils.go
@@ -4,11 +4,23 @@ import "time"
 
 type Validator struct {}
 
-func (v *Validator) IsValidMoney(value float64) bool {
+type TimeValidate interface {
+	IsValidDate(date time.Time) bool
+}
+
+func (Validator) IsValidDate(date time.Time) bool {
+	today := time.Now()
+	return date.After(today)
+}
+
+func (Validator) IsValidNotes(note float64) bool {
+	return note >= 0 && note <= 100
+}
+
+func (Validator) IsValidCash(value float64) bool {
 	return value > 0
 }
 
-func IsValidDate(date time.Time) bool {
-	today := time.Now()
-	return date.After(today)
+func  (Validator) IsValidYear(year int) bool {
+	return year > 1900 && year <=time.Now().Year()
 }
--- main.go
+++ main.go
@@ -6,24 +6,13 @@ import (
 	"time"
 )
 
-type Validator struct {}
-
-func (*Validator) IsValidNote(note float64) bool {
-	return note >= 0 && note <= 100
-}
-
-func IsValidYear(year int) bool {
-	return year > 1900 && year <=time.Now().Year()
-}
-
 func main() {
-	validator := new(Validator)
-	lvalidator := new(lib.Validator)
-	fmt.Println("Valid 10 = ", lvalidator.IsValidMoney(10))
-	fmt.Println("Valid -1 = ", lvalidator.IsValidMoney(-1))
-	fmt.Println("Valid 0 = ", lvalidator.IsValidMoney(0))
-	fmt.Println("Valid tomorrow = ", lib.IsValidDate(time.Now().AddDate(0,0,1)))
-	fmt.Println("Valid yesterday = ", lib.IsValidDate(time.Now().AddDate(0,0,-1)))
-	fmt.Println("Valid year = ", IsValidYear(2020))
-	fmt.Println("Valid note = ", validator.IsValidNote(99.9))
+	validator := new(lib.Validator)
+	fmt.Println("Valid 10 = ", validator.IsValidCash(10))
+	fmt.Println("Valid -1 = ", validator.IsValidCash(-1))
+	fmt.Println("Valid 0 = ", validator.IsValidCash(0))
+	fmt.Println("Valid tomorrow = ", validator.IsValidDate(time.Now().AddDate(0,0,1)))
+	fmt.Println("Valid yesterday = ", validator.IsValidDate(time.Now().AddDate(0,0,-1)))
+	fmt.Println("Valid year = ", validator.IsValidYear(2020))
+	fmt.Println("Valid note = ", validator.IsValidNotes(99.9))
 }
