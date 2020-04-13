2d426141ba9cf1f414bf355405a1a097062aa870
Rodrigo Brito <rig.10@hotmail.com>
2020-04-12 22:45:59 -0300

    example of refactoring

--- lib/utils.go
+++ lib/utils.go
@@ -1,5 +1,14 @@
 package lib
 
-func IsValidMoney(value float64) bool {
+import "time"
+
+type Validator struct {}
+
+func (v *Validator) IsValidMoney(value float64) bool {
 	return value > 0
+}
+
+func IsValidDate(date time.Time) bool {
+	today := time.Now()
+	return date.After(today)
 }
--- main.go
+++ main.go
@@ -6,15 +6,11 @@ import (
 	"time"
 )
 
-func IsValidDate(date time.Time) bool {
-	today := time.Now()
-	return date.After(today)
-}
-
 func main() {
-	fmt.Println("Valid 10 = ", lib.IsValidMoney(10))
-	fmt.Println("Valid -1 = ", lib.IsValidMoney(-1))
-	fmt.Println("Valid 0 = ", lib.IsValidMoney(0))
-	fmt.Println("Valid tomorrow = ", IsValidDate(time.Now().AddDate(0,0,1)))
-	fmt.Println("Valid yesterday = ", IsValidDate(time.Now().AddDate(0,0,-1)))
+	validator := new(lib.Validator)
+	fmt.Println("Valid 10 = ", validator.IsValidMoney(10))
+	fmt.Println("Valid -1 = ", validator.IsValidMoney(-1))
+	fmt.Println("Valid 0 = ", validator.IsValidMoney(0))
+	fmt.Println("Valid tomorrow = ", lib.IsValidDate(time.Now().AddDate(0,0,1)))
+	fmt.Println("Valid yesterday = ", lib.IsValidDate(time.Now().AddDate(0,0,-1)))
 }
