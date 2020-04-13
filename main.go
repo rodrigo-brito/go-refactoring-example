151f6418f430cfe535aa7c316aafce9aaec96010
Rodrigo Brito <rig.10@hotmail.com>
2020-04-13 20:09:25 -0300

    include new features

--- main.go
+++ main.go
@@ -6,11 +6,24 @@ import (
 	"time"
 )
 
+type Validator struct {}
+
+func (*Validator) IsValidNote(note float64) bool {
+	return note >= 0 && note <= 100
+}
+
+func IsValidYear(year int) bool {
+	return year > 1900 && year <=time.Now().Year()
+}
+
 func main() {
-	validator := new(lib.Validator)
-	fmt.Println("Valid 10 = ", validator.IsValidMoney(10))
-	fmt.Println("Valid -1 = ", validator.IsValidMoney(-1))
-	fmt.Println("Valid 0 = ", validator.IsValidMoney(0))
+	validator := new(Validator)
+	lvalidator := new(lib.Validator)
+	fmt.Println("Valid 10 = ", lvalidator.IsValidMoney(10))
+	fmt.Println("Valid -1 = ", lvalidator.IsValidMoney(-1))
+	fmt.Println("Valid 0 = ", lvalidator.IsValidMoney(0))
 	fmt.Println("Valid tomorrow = ", lib.IsValidDate(time.Now().AddDate(0,0,1)))
 	fmt.Println("Valid yesterday = ", lib.IsValidDate(time.Now().AddDate(0,0,-1)))
+	fmt.Println("Valid year = ", IsValidYear(2020))
+	fmt.Println("Valid note = ", validator.IsValidNote(99.9))
 }
